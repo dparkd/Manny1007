83e1ed9cfb347f68ce5140d7b3ef56466b53a66a
dparkd <d.hanul.park@gmail.com>
2021-12-24 14:01:05 -0500

    santa

---
+++ components/MannyIntro.js
@@ -0,0 +1,9 @@
+export default function MannyIntro ({ title }) {
+  return (
+    <div className="player">
+      <img  src="/player.png"></img>
+      <video src="/santa.mp4" autoPlay muted></video>
+    </div>
+
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,17 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import MannyIntro from '@components/MannyIntro'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Manny1007 Studio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <MannyIntro title="Welcome to my app!" />
       </main>
-
-      <Footer />
     </div>
   )
 }
